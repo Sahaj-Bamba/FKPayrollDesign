51ad270397ad6be2f5eea54e12f223ed7f932f5b
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-05-10 02:22:04 +0530

    Started working on union implementation 		Created union main menu

--- Source/src/com/Sahaj/UX/Union.java
+++ Source/src/com/Sahaj/UX/Union.java
@@ -1,7 +1,44 @@
 package com.Sahaj.UX;
 
-public class Union {
+public class Union extends Menu{
 
-	//@Todo: Union menu stuff do latter
+	public Union(){
+		super();
+		start();
+	}
+
+	@Override
+	protected void init() {
+		choices.add("Add Union");
+		choices.add("Add Employee");
+		choices.add("Update Fees");
+		choices.add("Go Back");
+	}
+
+	@Override
+	protected void makeMove(int choice) {
+		switch (choice){
+			case 1: addUnion();
+				break;
+			case 2: addEmployee();
+				break;
+			case 3: updateFee();
+				break;
+			case 4: goBack();
+				break;
+		}
+	}
+
+	private void updateFee() {
+
+	}
+
+	private void addEmployee() {
+		
+	}
+
+	private void addUnion() {
+		
+	}
 
 }
